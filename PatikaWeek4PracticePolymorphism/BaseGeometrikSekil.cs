62dfb3df2fb33a0ea586009bbf5bcaa10025897f
Bilal <bilalbilin12@gmail.com>
2024-08-08 14:21:56 +0300

    Add project files.

---
+++ PatikaWeek4PracticePolymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PracticePolymorphism
+{
+    public class BaseGeometrikSekil // Creating a new class
+    {
+        public double Width { get; set; } // Defining properties
+
+        public double Height { get; set; }
+
+        
+        public virtual void AreaCalculation() // Creating a virtual method that calculates area
+        {
+            Console.WriteLine($"Verilen şeklin alanı --> {Width * Height}");
+        }
+
+    }
+
+    public class Kare() : BaseGeometrikSekil // Creating a new class that inherits from base class
+    {
+        public override void AreaCalculation() // Overriding first method for Kare class
+        {
+            Console.WriteLine($"Karenin alanı --> {Width * Height}");
+        }
+    }
+
+    public class Dikdortgen() : BaseGeometrikSekil // Creating a new class that inherits from base class
+    {
+        public override void AreaCalculation() // Overriding first method for Dikdortgen class
+        {
+            Console.WriteLine($"Dikdörtgenin alanı --> {Width * Height}");
+        }
+    }
+
+    public class DikUcgen() : BaseGeometrikSekil // Creating a new class that inherits from base class
+    {
+        public override void AreaCalculation() // Overriding first method for DikUcgen class
+        {
+            Console.WriteLine($"Dik üçgenin alanı --> {(Width * Height) / 2}");
+        }
+    }
+}
---
+++ PatikaWeek4PracticePolymorphism/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using PatikaWeek4PracticePolymorphism;
+
+Kare kare = new Kare(); // Creating an instance of Kare object
+
+// Setting properties
+kare.Width = 6; 
+kare.Height = 6;
+
+// Display area information
+kare.AreaCalculation();
+
+Dikdortgen dikdortgen = new Dikdortgen(); // Creating an instance of Dikdortgen object
+
+//Setting properties
+dikdortgen.Width = 8;
+dikdortgen.Height = 3;
+
+// Display area information
+dikdortgen.AreaCalculation();
+
+DikUcgen dikUcgen = new DikUcgen(); // Creating an instance of DikUcgen object
+
+//Setting properties
+dikUcgen.Width = 7;
+dikUcgen.Height = 9;
+
+//Display area information
+dikUcgen.AreaCalculation();
+
